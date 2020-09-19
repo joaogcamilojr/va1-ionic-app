9c606633124263802064340924055ee4047a867e
João Gustavo Camilos Júnior <joao.camilo.jr@outlook.com>
2020-09-19 15:12:18 -0300

    feat: make page about

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import music from '../../assets/images/music.jpg'
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
   styleUrls: ['./about.page.scss'],
 })
 export class AboutPage implements OnInit {
+	public image = [];
 
   constructor() { }
 
   ngOnInit() {
+		this.image = [{
+			'img': music,
+		}]
   }
 
 }
